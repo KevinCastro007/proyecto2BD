b5f2ecf9d5ab589610666eb00d937e6237251f6e
Kevin Castro <castrokevin7@gmail.com>
2015-05-29 02:40:03 -0600

    Web app initialization

---
+++ Aplicacion/web/public/controller/main.js
@@ -0,0 +1,73 @@
+
+// Controladores de las vistas de la página web
+var myApp = angular.module('myApp', ['ngRoute']);		//Exportación del módulo.
+
+// Configuración de las rutas (web views) con sus respectivos controladores
+myApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '../pages/home.html',
+			controller 	: 'mainController'
+		})
+		.when('/activity', {
+			templateUrl : '../pages/activity.html',
+			controller 	: 'activityController'
+		})
+		.when('/supply', {
+			templateUrl : '../pages/supply.html',
+			controller 	: 'supplyController'
+		})
+		.when('/machinery', {
+			templateUrl : '../pages/machinery.html',
+			controller 	: 'machineryController'
+		})
+		.when('/service', {
+			templateUrl : '../pages/service.html',
+			controller 	: 'serviceController'
+		})
+		.otherwise({
+			redirectTo : '/'
+		});
+});
+//Main Controller
+myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) {
+	$scope.message = function() {
+		alert("Hola Usuario, desde el Main Controller!");
+	}
+}]);
+
+//Activity Controller
+myApp.controller('activityController', ['$scope', '$http', function ($scope, $http) {
+	$scope.propertySelection = function() {
+		$http.get('/lots/' + $scope.history.property.ID).success(function (response) {
+			$scope.lots = response;
+		});	
+	}
+	$scope.lotSelection = function() {
+		alert("OKA: " + $scope.history.lot.ID);
+	}	
+	$http.get('/properties').success(function (response) {
+		$scope.properties = response;	
+	});	
+}]);
+
+//Supply Controller
+myApp.controller('supplyController', ['$scope', '$http', function ($scope, $http) {
+	$scope.message = function() {
+		alert("Hola Usuario, desde el Supply Controller!");
+	}
+}]);
+
+//Machinery Controller
+myApp.controller('machineryController', ['$scope', '$http', function ($scope, $http) {
+	$scope.message = function() {
+		alert("Hola Usuario, desde el Machinery Controller!");
+	}
+}]);
+
+//Service Controller
+myApp.controller('serviceController', ['$scope', '$http', function ($scope, $http) {
+	$scope.message = function() {
+		alert("Hola Usuario, desde el Service Controller!");
+	}
+}]);
