f16b41bec8dfc07f1c69e2ae020d8d644fe833d8
Kevin Castro <castrokevin7@gmail.com>
2015-05-31 11:50:15 -0600

    Web

--- Aplicacion/web/public/controller/main.js
+++ Aplicacion/web/public/controller/main.js
@@ -35,27 +35,28 @@ var fk_LotXCycle = 0;
 myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) {	
 	$scope.salir = function () {
 		window.location = ("/index.html"); 
-	}
+	};
 	$scope.propertySelection = function() {
 		$scope.lots = null;
 		$scope.cycles = null;
 		$http.get('/lots/' + $scope.history.property.ID).success(function (response) {
 			$scope.lots = response;
 		});	
-	}
+	};
 	$scope.lotSelection = function() {
 		$scope.cycles = null;
 		$http.get('/cycles/' + $scope.history.lot.ID).success(function (response) {
 			$scope.cycles = response;
 		});			
-	}	
-	$scope.cycleSelection = function() {
+	};	
+	$scope.cycleSelection = function () {
 		var IDs = [$scope.history.lot.ID, $scope.history.cycle.ID];
 		$http.get('/lotXCycle/' + IDs).success(function (response) {
 			fk_LotXCycle = response;
+			alert(fk_LotXCycle);
 		});		
 		window.location = ("/main.html");
-	}		
+	};		
 	$http.get('/properties').success(function (response) {
 		$scope.properties = response;	
 	});	
@@ -69,31 +70,88 @@ myApp.controller('historyController', ['$scope', '$http', function ($scope, $htt
 }]);
 
 //Supply Controller
-myApp.controller('supplyController', ['$scope', '$http', function ($scope, $http) {
-	$http.get('/supplies').success(function (response) {
-		$scope.supplies = response;	
-	});	
-	$http.get('/activities').success(function (response) {
-		$scope.activities = response;	
-	});
+myApp.controller('supplyController', ['$scope', '$http', function ($scope, $http) {	
+	var refresh = function () {
+		$http.get('/supplies').success(function (response) {
+			$scope.supplies = response;	
+		});	
+		$http.get('/activities').success(function (response) {
+			$scope.activities = response;	
+		});	
+		$scope.supplyRequest = "";		
+	};
+	$scope.proceed = function () {
+		if (typeof($scope.supplyRequest.activity) === 'undefined') {
+			alert("Seleccione la Actividad!");
+		}
+		else if (typeof($scope.supplyRequest.supply) === 'undefined') {
+			alert("Seleccione el Suministro!");
+		}	
+		else if (typeof($scope.supplyRequest.state) === 'undefined') {
+			alert("Seleccione el Estado!");
+		}		
+		else {	
+			console.log(fk_LotXCycle);
+			console.log($scope.supplyRequest);		
+		}
+	};
+	refresh();
 }]);
 
 //Machinery Controller
 myApp.controller('machineryController', ['$scope', '$http', function ($scope, $http) {
-	$http.get('/machinery').success(function (response) {
-		$scope.machinery = response;	
-	});	
-	$http.get('/activities').success(function (response) {
-		$scope.activities = response;	
-	});
+	var refresh = function () {
+		$http.get('/machinery').success(function (response) {
+			$scope.machinery = response;	
+		});	
+		$http.get('/activities').success(function (response) {
+			$scope.activities = response;	
+		});	
+		$scope.machineryRequest = "";		
+	};
+	$scope.proceed = function () {
+		if (typeof($scope.machineryRequest.activity) === 'undefined') {
+			alert("Seleccione la Actividad!");
+		}
+		else if (typeof($scope.machineryRequest.machine) === 'undefined') {
+			alert("Seleccione la Máquina!");
+		}	
+		else if (typeof($scope.machineryRequest.state) === 'undefined') {
+			alert("Seleccione el Estado!");
+		}		
+		else {	
+			console.log(fk_LotXCycle);
+			console.log($scope.machineryRequest);		
+		}
+	};
+	refresh();
 }]);
 
 //Service Controller
 myApp.controller('serviceController', ['$scope', '$http', function ($scope, $http) {
-	$http.get('/services').success(function (response) {
-		$scope.services = response;	
-	});	
-	$http.get('/activities').success(function (response) {
-		$scope.activities = response;	
-	});
+	var refresh = function () {
+		$http.get('/services').success(function (response) {
+			$scope.services = response;	
+		});	
+		$http.get('/activities').success(function (response) {
+			$scope.activities = response;	
+		});	
+		$scope.serviceRequest = "";		
+	};
+	$scope.proceed = function () {
+		if (typeof($scope.serviceRequest.activity) === 'undefined') {
+			alert("Seleccione la Actividad!");
+		}
+		else if (typeof($scope.serviceRequest.service) === 'undefined') {
+			alert("Seleccione el Servicio!");
+		}	
+		else if (typeof($scope.serviceRequest.state) === 'undefined') {
+			alert("Seleccione el Estado!");
+		}		
+		else {	
+			console.log(fk_LotXCycle);
+			console.log($scope.serviceRequest);		
+		}
+	};
+	refresh();
 }]);
---
+++ SQLScripts_Proyecto2/StoreProcedures/Request.sql
@@ -0,0 +1,69 @@
+/* Stored Procedures related with: Request */
+
+GO
+USE AgriculturalProperty
+
+GO
+CREATE PROCEDURE APSP_InsertRequest(@FK_LotXCycle INT,  @FK_ActivityType VARCHAR(50), @RequestType VARCHAR(50), @Date DATETIME, @Description VARCHAR(150), @Request VARCHAR(50), @Amount FLOAT, @State INT)
+AS
+BEGIN
+	BEGIN TRY
+		IF (dbo.APFN_RequestTypeID(@RequestType) <> 0 and dbo.APFN_RequestTypeManagerID(@RequestType) <> 0
+			and dbo.APFN_LotID(@LotCode) <> 0 and dbo.APFN_CycleID(@CycleStart, @CycleEnd) and @Amount > 1) 
+		BEGIN
+			DECLARE @RequestID INT
+			SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+			BEGIN TRANSACTION
+				INSERT INTO dbo.AP_Request(FK_RequestManager, FK_LotXCycle, FK_RequestType, RequestDescription, State) 
+					VALUES(dbo.APFN_RequestTypeManagerID(@RequestType), dbo.APFN_LotXCycleID(dbo.APFN_LotID(@LotCode), 
+						dbo.APFN_CycleID(@CycleStart, @CycleEnd)), dbo.APFN_RequestTypeID(@RequestType), @Description, @State)
+
+				@RequestID = SCOPE_IDENTITY()
+				
+				dbo.APSP_InsertActivityType(@ActivityType)
+
+				INSERT INTO dbo.AP_HistoricalActivity(FK_ActivityType, FK_Request, ActivityDate, ActivityDescription)
+					VALUES(dbo.APFN_ActivityTypeID(@ActivityType), @RequestID, @Date, @Description)
+
+				IF (@RequestType = 'Servicio')
+				BEGIN
+					IF (dbo.APFN_ServiceID(@Request) <> 0)
+					BEGIN	
+						INSERT INTO dbo.AP_ServiceRequest(FK_Service, RequestID, AmountHours) 
+							VALUES(dbo.APFN_ServiceID(@Request), @RequestID, @Amount)
+						RETURN 1
+					END
+					ELSE
+						RETURN 0
+				END
+				IF (@RequestType = 'Suministro')
+				BEGIN 
+					IF (dbo.APFN_SupplyQuantity(@Request) <> 0)
+					BEGIN	
+						INSERT INTO dbo.AP_SupplyRequest(FK_Service, RequestID, Amount) 
+							VALUES(dbo.APFN_SupplyID(@Request), @RequestID, @Amount)
+						RETURN 1
+					END
+					ELSE
+						RETURN 0
+				END
+				ELSE
+				BEGIN
+					IF (dbo.APFN_MachineryQuantity(@Request) <> 0)
+					BEGIN	
+						INSERT INTO dbo.AP_MachineryRequest(FK_Service, RequestID, AmountHours) 
+							VALUES(dbo.APFN_MachineryID(@Request), @RequestID, @Amount)
+						RETURN 1
+					END
+					ELSE
+						RETURN 0
+				END
+			COMMIT
+		END
+		ELSE
+			RETURN 0
+	END TRY
+	BEGIN CATCH
+		RETURN @@ERROR * -1
+	END CATCH
+END
